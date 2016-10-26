c8c16ef480112d2acc904883e6e165890de60cb1
joetancy <joetanchinyong@gmail.com>
2016-10-26 14:18:49 +0800

    Models for JSON
    Updated models for json data

--- cz3003ssadBack/backend/models.py
+++ cz3003ssadBack/backend/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Crisis(models.Model):
+    disaster = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    shapeType = models.CharField(max_length=10)
+    description = models.CharField(max_length=300)
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+       return self.name
+
+class CrisisCoordinates(models.Model):
+    crisis = models.ForeignKey(Crisis, on_delete=models.CASCADE)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+       return self.crisis.name
