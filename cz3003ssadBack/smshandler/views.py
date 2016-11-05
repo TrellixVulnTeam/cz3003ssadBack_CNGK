a2ab8efb3848dc133c7abaa5094d5753ebd6233d
joetancy <joetanchinyong@gmail.com>
2016-11-05 16:21:42 +0800

    updated sms

--- cz3003ssadBack/smshandler/views.py
+++ cz3003ssadBack/smshandler/views.py
@@ -6,14 +6,29 @@ import requests
 
 # Create your views here.
 def index(request):
+	postdata = {
+    'device': settings.SMS_ID,
+    'email': settings.SMS_EMAIL,
+    'password': settings.SMS_PW,
+    'number': '91718170',
+    'message': "AUTO MESSAGE, LET ME KNOW IF YOU SEE IT",
+	}
+	r = requests.post(settings.SMS_GATEWAY_URL, data=postdata)
+	print(r.status_code)
+	print(r.text)
+	return HttpResponse("Hello, world. You're at the sms index.")
+
+
+def generateSms(request):
+	received_json_data = json.loads(request.body.decode('utf-8'))
+	send_to = received_json_data["send_to"]
+    message_body = received_json_data["message_body"]
     postdata = {
-        'device': settings.SMS_ID,
-        'email': settings.SMS_EMAIL,
-        'password': settings.SMS_PW,
-        'number': '91007606',
-        'message': "AUTO MESSAGE, LET ME KNOW IF YOU SEE IT",
-    }
-    r = requests.post(settings.SMS_GATEWAY_URL, data=postdata)
-    print(r.status_code)
-    print(r.text)
-    return HttpResponse("Hello, world. You're at the sms index.")
+    'device': settings.SMS_ID,
+    'email': settings.SMS_EMAIL,
+    'password': settings.SMS_PW,
+    'number': send_to,
+    'message': message_body,
+	}
+	r = requests.post(settings.SMS_GATEWAY_URL, data=postdata)
+	return HttpResponse(received_json_data)
