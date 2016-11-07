67e27c6eb2b7da72b1d3421071ec5fbb2cd7c3b9
joetancy <joetanchinyong@gmail.com>
2016-11-07 19:12:50 +0800

    emailtimer

--- cz3003ssadBack/email_handler/views.py
+++ cz3003ssadBack/email_handler/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.conf import settings
+from datetime import datetime
 
 
 # Create your views here.
 def index(request):
-	result = send_mail('Subject here', 'Here is the message.', settings.EMAIL_HOST_USER,['timothy_lee@outlook.com'], fail_silently=False)
-	return HttpResponse(result)
+    result = send_mail('Subject here', 'Here is the message. Time: ' + str(datetime.now()), settings.EMAIL_HOST_USER, [
+                       'timothy_lee@outlook.com'], fail_silently=False)
+    print(result)
+    return HttpResponse(result)
